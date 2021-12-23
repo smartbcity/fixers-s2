bef350942bf59f615cae58ec584154bf37a9c38b
Adrien Poupard <adrien@smartb.city>
2021-12-23 14:29:14 +0100

    Fix storybook build

--- docs/.storybook/webpack.config.js
+++ docs/.storybook/webpack.config.js
@@ -1,18 +1,18 @@
-const renderer = `
-import React from 'react'
-import {mdx} from '/node_modules/@mdx-js/react'
-`
-
-module.exports = ({config}) => {
-//    config.module.rules[2].use[1].options = {...config.module.rules[2].use[1].options, renderer}
-    const assetRule = config.module.rules.find(({ test }) => test.test('.svg'));
-    const assetLoader = {
-        loader: assetRule.loader,
-        options: assetRule.options || assetRule.query,
-    };
-    config.module.rules.unshift({
-        test: /\.svg$/,
-        use: ['@svgr/webpack', assetLoader],
-    });
-    return config;
-}
+// const renderer = `
+// import React from 'react'
+// import {mdx} from '/node_modules/@mdx-js/react'
+// `
+//
+// module.exports = ({config}) => {
+// //    config.module.rules[2].use[1].options = {...config.module.rules[2].use[1].options, renderer}
+//     const assetRule = config.module.rules.find(({ test }) => test.test('.svg'));
+//     const assetLoader = {
+//         loader: assetRule.loader,
+//         options: assetRule.options || assetRule.query,
+//     };
+//     config.module.rules.unshift({
+//         test: /\.svg$/,
+//         use: ['@svgr/webpack', assetLoader],
+//     });
+//     return config;
+// }
