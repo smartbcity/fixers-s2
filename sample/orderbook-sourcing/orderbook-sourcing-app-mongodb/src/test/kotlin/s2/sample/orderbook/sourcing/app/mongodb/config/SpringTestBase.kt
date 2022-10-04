be3d8f308f85f357bd6016dc64e68f2859c7920d
Adrien Poupard <adrien@smartb.city>
2022-10-04 23:47:08 +0200

    Use testcontainers to start redis

--- sample/orderbook-sourcing/orderbook-sourcing-app-mongodb/src/test/kotlin/s2/sample/orderbook/sourcing/app/mongodb/config/SpringTestBase.kt
+++ sample/orderbook-sourcing/orderbook-sourcing-app-mongodb/src/test/kotlin/s2/sample/orderbook/sourcing/app/mongodb/config/SpringTestBase.kt
@@ -3,9 +3,14 @@ package s2.sample.orderbook.sourcing.app.mongodb.config
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.util.TestPropertyValues
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.MongoDBContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -15,23 +20,29 @@ import s2.sample.orderbook.sourcing.app.mongodb.SubAutomateMongodbApp
 @Testcontainers
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(SpringExtension::class)
+@ContextConfiguration(initializers = [SpringTestBase.Initializer::class])
 @SpringBootTest(classes = [SubAutomateMongodbApp::class])
 abstract class SpringTestBase {
 
 	companion object {
-		@JvmStatic
-		@Container
 		val mongoContainer: MongoDBContainer =
-			MongoDBContainer(DockerImageName.parse("mongo:4.4")).apply {
-				start()
-			}
+			MongoDBContainer(DockerImageName.parse("mongo:4.4"))
 
-		@JvmStatic
-		@DynamicPropertySource
-		fun setProperties(registry: DynamicPropertyRegistry) {
-			registry.add("spring.data.mongodb.uri", mongoContainer::getReplicaSetUrl)
-		}
-
+		val redisContainer = GenericContainer<Nothing>("redis/redis-stack:6.2.2-v2-edge")
+			.apply { withExposedPorts(6379) }
 	}
 
+	internal class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+		override fun initialize(configurableApplicationContext: ConfigurableApplicationContext) {
+			redisContainer.start()
+			mongoContainer.start()
+			TestPropertyValues.of(
+				"spring.data.mongodb.uri=${mongoContainer.replicaSetUrl}",
+				"spring.redis.host=${redisContainer.host}",
+				"spring.redis.port=${redisContainer.firstMappedPort}"
+			).applyTo(configurableApplicationContext.environment)
+		}
+
+
+	}
 }
